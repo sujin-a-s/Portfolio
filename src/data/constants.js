b0e92d6cb8e311fa52947d28bca557a8fe0d3692
Sujin <sunilkumarsujin@gmail.com>
2024-08-15 15:47:55 +0530

    portfolio

---
+++ src/data/constants.js
@@ -0,0 +1,350 @@
+
+
+export const Bio = {
+  name: "Sujin",
+  roles: [
+    "Full Stack Developer",
+    "Next JS Developer",
+    
+  ],
+  description:
+    "Welcome to my realm of full-stack development, where innovation and technology converge. As a dedicated developer, I excel in creating dynamic and efficient applications that span both front-end and back-end. With a focus on delivering exceptional user experiences and leveraging modern technologies, I turn ideas into robust digital solutions. Dive into my portfolio to see how I blend creativity with technical expertise to build seamless, end-to-end applications.",
+  github: "https://github.com/sujin-a-s",
+  resume:
+    "https://drive.google.com/file/d/1mu9RRXOZ58fAClKKKduymSE5oYNkpyig/view?usp=drive_link",
+  linkedin: "https://www.linkedin.com/in/sujin-a-s/",
+
+
+};
+
+export const skills = [
+  {
+    title: "Frontend",
+    skills: [
+      {
+        name: "React JS",
+        image:
+          "https://upload.wikimedia.org/wikipedia/commons/thumb/a/a7/React-icon.svg/1200px-React-icon.svg.png",
+      },
+      {
+        name: "Redux Toolkit",
+        image:
+          "https://d33wubrfki0l68.cloudfront.net/0834d0215db51e91525a25acf97433051f280f2f/c30f5/img/redux.svg",
+      },
+      {
+        name: "Tailwind CSS",
+        image:
+          "https://upload.wikimedia.org/wikipedia/commons/thumb/d/d5/Tailwind_CSS_Logo.svg/512px-Tailwind_CSS_Logo.svg.png?20230715030042",
+      },
+      {
+        name: "HTML",
+        image: "https://www.w3.org/html/logo/badge/html5-badge-h-solo.png",
+      },
+      {
+        name: "CSS",
+        image:
+          "https://upload.wikimedia.org/wikipedia/commons/thumb/d/d5/CSS3_logo_and_wordmark.svg/1452px-CSS3_logo_and_wordmark.svg.png",
+      },
+      {
+        name: "JavaScript",
+        image:
+          "https://upload.wikimedia.org/wikipedia/commons/thumb/6/6a/JavaScript-logo.png/800px-JavaScript-logo.png",
+      },
+      {
+        name: "TypeScript",
+        image:
+          "https://tse2.mm.bing.net/th?id=OIP.maKe3jXsLd8flovNsX2_3QHaHa&pid=Api&P=0&h=180",
+      },
+    ],
+  },
+
+  {
+    title: "Backend",
+    skills: [
+      {
+        name: "Express JS",
+        image:
+          "https://expressjs.com/images/express-facebook-share.png",
+      },
+      {
+        name: "Prisma",
+        image:
+          "https://tse1.mm.bing.net/th?id=OIP.dz11dyL9Qqg2JQMyDjZAtAAAAA&pid=Api&P=0&h=180",
+      },
+      {
+        name: "NextAuth",
+        image:
+          "https://next-auth.js.org/img/logo/logo.png",
+      },
+    ],
+  },
+
+  {
+    title: "Database Technologies",
+    skills: [
+      {
+        name: "PostgreSQL",
+        image:
+          "https://upload.wikimedia.org/wikipedia/commons/2/29/Postgresql_elephant.svg",
+      },
+      {
+        name: "MongoDB",
+        image:
+          "https://tse4.mm.bing.net/th?id=OIP.mwJKYF6LLQ7U0_YKqdCATwHaEo&pid=Api&P=0&h=180",
+      },
+    ],
+  },
+
+  {
+    title: "Full Stack",
+    skills: [
+      {
+        name: "Next.js",
+        image:
+          "https://tse4.mm.bing.net/th?id=OIP.d-cssZMmcDWJU_yKxt9abQHaFQ&pid=Api&P=0&h=180",
+      },
+    ],
+  },
+
+  {
+    title: "Developer Tools and Technologies",
+    skills: [
+      {
+        name: "Git",
+        image:
+          "https://tse2.mm.bing.net/th?id=OIP.q0DQLwqov4XeEv1FvaWwwAHaHa&pid=Api&P=0&h=180",
+      },
+      {
+        name: "GitHub",
+        image:
+          "https://github.githubassets.com/images/modules/logos_page/GitHub-Mark.png",
+      },
+
+    ],
+  },
+];
+
+
+export const experiences = [
+  {
+    id: 0,
+    img: "https://media.licdn.com/dms/image/D560BAQFDUKPSU8P0Lw/company-logo_200_200/0/1687947132305?e=1698278400&v=beta&t=cBF3SdszRLspmyvV-rHa1y4tdj_hjjivH7Xy0Z7kOD8",
+    role: "DevOps & Fullstack Engineering Intern",
+    company: "Flipr Inovations Pvt. Ltd.",
+    date: "Aug 2023 - July 2023",
+    desc: "Working on Flipr Platforms, managing DevOps, and streamlining the process with automation.",
+    skills: [
+      "Docker",
+      "Terraform",
+      "AWS",
+      "EC2",
+      "Portainer",
+      "Nginx",
+      "JavaScript",
+      "TypeScript",
+      "Node Js",
+      " Next Js",
+    ],
+    doc: "https://media.licdn.com/dms/image/D4D2DAQFlp60ZqHuaFQ/profile-treasury-image-shrink_1280_1280/0/1691180828512?e=1692381600&v=beta&t=mM5Y_NE5EPlQhez5FAN6NLVSKcO_Ojt_9Gq3mnFGkAQ",
+  },
+  {
+    id: 0,
+    img: "https://media.licdn.com/dms/image/C560BAQFQb9_Lb3bZug/company-logo_200_200/0/1644414857251?e=1693440000&v=beta&t=7WqhIiUCg_0otFDwWn-O_SP-Z4Ftoe4be1WlnjYoESc",
+    role: "Frontend Engineer Intern",
+    company: "Neurobit",
+    date: "June 2023 - Present",
+    desc: "Working on the frontend of the web application using ReactJS, Redux, and Material UI.",
+    skills: [
+      "ReactJS",
+      "Redux",
+      "NodeJs",
+      "Material UI",
+      "HTML",
+      "CSS",
+      "JavaScript",
+    ],
+    doc: "https://firebasestorage.googleapis.com/v0/b/buckoid-917cf.appspot.com/o/Screenshot%20from%202023-05-28%2023-20-46.png?alt=media&token=5570f995-e8f4-4f12-bb2f-73bcf4b20730",
+  },
+  {
+    id: 1,
+    img: "https://media.licdn.com/dms/image/D560BAQFDUKPSU8P0Lw/company-logo_200_200/0/1687947132305?e=1698278400&v=beta&t=cBF3SdszRLspmyvV-rHa1y4tdj_hjjivH7Xy0Z7kOD8",
+    role: "Fullstack Externship",
+    company: "Flipr",
+    date: "June 2023 - July 2023",
+    desc: "Built an employee management full stack web app used Docker and deployed on AWS ec2. I was the top performer in the program.",
+    skills: [
+      "ReactJS",
+      "Redux",
+      "NodeJs",
+      "Material UI",
+      "HTML",
+      "CSS",
+      "JavaScript",
+      "Docker",
+      "AWS",
+      "MongoDB",
+    ],
+  },
+  {
+    id: 2,
+    img: "https://media.licdn.com/dms/image/C510BAQGSObrO0QPlMQ/company-logo_200_200/0/1555704980721?e=1692835200&v=beta&t=FjOWcUVVNzqFbTmmp0iXQkv5keBQC5lxIQ7K6rmMSMQ",
+    role: "Open Source Contributor ",
+    company: "GirlScript Summer of Code",
+    date: "May 2023 - Present",
+    desc: "Contributing to different open-source projects and learn from industry experts",
+    doc: "https://media.licdn.com/dms/image/D4D22AQGzSpgFaR4hsQ/feedshare-shrink_800/0/1684079632872?e=1687996800&v=beta&t=i0xWn_k32Oj9SxKfc6rARSbOghXD9Ge6WyEb2p2vvrc",
+  },
+  {
+    id: 3,
+    img: "https://media.licdn.com/dms/image/D4D0BAQGfGQHEJt8gPA/company-logo_100_100/0/1666002815185?e=1692230400&v=beta&t=c4obZda4Uh9q6vOcmCbxjoSiExvbvYwEHUlgdY0tU-4",
+    role: "Android Developer",
+    company: "DSC KIIT",
+    date: "Nov2021 - Present",
+    desc: "As an Android developer at the Google Developers Student Club (GDCS), I have had the opportunity to work on exciting projects and collaborate with talented developers who share my passion for technology. Through my involvement with GDCS, I have also had the opportunity to host and participate in numerous events, including hackathons, study jams, and workshops.",
+    skills: [
+      "Leadership",
+      "Mobile Application Development",
+      "Kotlin",
+      "XML",
+      "Figma",
+    ],
+  },
+  {
+    id: 4,
+    img: "https://media.licdn.com/dms/image/C4D0BAQH4od9HG2K7bw/company-logo_100_100/0/1661484397513?e=1692230400&v=beta&t=IW-eebE0PpFxqZ8Q3UCYYtlAokbhAEqsNePBv2Pmg18",
+    role: "Android Developer Intern",
+    company: "Rudraksha Welffare Foundation",
+    date: "June 2021 - Oct 2021",
+    desc: "As an Android Developer Intern at Rudraksha Welfare Foundation from June 2021 to October 2021, I gained valuable hands-on experience in application development. During my tenure, I contributed to the development of the application's back-end cloud functions using Firebase and implemented a front-end with the MVVM structure, which was designed using Figma. I also added a one-to-one video call feature, conceptualized new features, and optimized app performance. This internship helped me enhance my skills in Android development and gave me an opportunity to work in a collaborative team environment.",
+    skills: [
+      "Android",
+      "Java",
+      "Kotlin",
+      "XML",
+      "Node Js",
+      "Cloud Firestore",
+      "Firebase",
+      "Figma",
+    ],
+    doc: "https://firebasestorage.googleapis.com/v0/b/buckoid-917cf.appspot.com/o/WhatsApp%20Image%202023-05-05%20at%2012.07.39%20AM.jpeg?alt=media&token=9f0e1648-568b-422d-bd0b-1f125f722245",
+  },
+];
+
+export const education = [
+  {
+    id: 0,
+    img: "https://tse1.mm.bing.net/th?id=OIP.cn0AeZtsc54VyqYhQ5HrJQAAAA&pid=Api&P=0&h=180",
+    school: "Hindusthan College of Engineering and Technology, Coimbatore",
+    date: "2020 - 2024",
+    grade: "8.5 CGPA",
+    desc: "I completed my bachelor's degree in Electronics and Communication Engineering in May 2024 from a respected institution. Though my major was in ECE, I developed a strong interest in information technology and coding along the way. With the support of dedicated faculty, I gained valuable technical skills and honed my problem-solving abilities. This journey fueled my passion for technology and solidified my commitment to excellence in the IT field. I am truly grateful for the education I received, which has provided me with the knowledge and foundation I need to build a successful career in IT.",
+
+   
+    degree: "Bachelor of Engineering - B.E, Electronics and Communication Engineering",
+  },
+  {
+    id: 1,
+    img: "https://www.joonsquare.com/usermanage/image/business/ksr-matriculation-higher-secondary-school-namakkal-10308/ksr-matriculation-higher-secondary-school-namakkal-logo.jpg",
+    school: "KSR Matriculation Higher Secondary School, Tiruchengode",
+    date: "2018 - 2019",
+    grade: "90.5%",
+    desc: "I completed my high school education at KSR Matriculation Higher Secondary School in Tiruchengode, where I pursued the Computer Science and Mathematics group.",
+    degree: "Computer Science ",
+  },
+  
+]; 
+
+export const projects = [
+  {
+    id: 9,
+    title: "Swiggy Clone",
+    date: "August 2023",
+    description:
+      "I harnessed the power of React JS to craft an engaging user interface and used Redux Toolkit to streamline state management. To bring that extra flair, I employed the magic of Tailwind CSS for captivating styling and made navigation a breeze with React Router. Notable features include a captivating Shimmer UI, a robust search function, advanced filtering, and an intuitive 'Add to Cart' option. I ensured smooth operation with intelligent network checks, complete responsiveness across devices.",
+    image:
+      "https://st.adda247.com/https://wpassets.adda247.com/wp-content/uploads/multisite/sites/5/2022/05/16165408/swiggy.jpeg",
+    tags: [
+      "React",
+      "Redux",
+      "Tailwind CSS",
+      "React-Router",
+    ],
+    category: "web app",
+    webapp: "https://swiggy-clone-self.vercel.app/",
+  },
+  
+  {
+    id: 1,
+    title: "YouTube Clone",
+    date: "Sep 2023",
+    description:
+      "I built an engaging video streaming platform using React, Redux, and Redux Toolkit, ensuring a seamless viewing experience. The design is modern and user-friendly, thanks to Tailwind CSS, while React Query supercharged performance by reducing network delays. I seamlessly integrated the YouTube API, giving users access to a vast library of videos, just like having YouTube at their fingertips! I optimized the search feature with debouncing for lightning-fast results and added real-time chat for an interactive viewing experience.",
+    image:
+      "https://images.indianexpress.com/2023/05/youtube-logo-featured.jpg",
+    tags: [
+      "React JS",
+      "React-Router",
+      "Tailwind CSS",
+      "Redux",
+    ],
+    category: "web app",
+    webapp: "https://youtube-clone-nu-nine.vercel.app/",
+  },
+
+  {
+    id: 2,
+    title: "Netflix GPT",
+    date: "Jan 2024",
+    description:
+      "This project is a Netflix clone powered by GPT (Generative Pre-trained Transformer) for movie suggestions. It includes user authentication, a browse page with trailers, main movies, and suggestions. The GPT feature allows users to search for movie recommendations using a tailored search bar. The project is built with React, Tailwind CSS, and integrates with TMDB API for movie data.",
+    image:
+      "https://s.yimg.com/fz/api/res/1.2/XYLv14Ua5rt2RM6s39IHow--~C/YXBwaWQ9c3JjaGRkO2ZpPWZpdDtoPTI0MDtxPTgwO3c9MjQw/https://s.yimg.com/zb/imgv1/b696b8bc-fcfd-3123-83cf-5dfa4c0a3bee/t_500x300",
+    tags: [
+      "React JS",
+      "React-Router",
+      "Tailwind CSS",
+      "Redux",
+    ],
+    category: "web app",
+    webapp: "https://netflix-gpt-smoky-nine.vercel.app/",
+  },
+
+  {
+    id: 3,
+    title: "Blog App",
+    date: "Mar 2024",
+    description:
+      "Developed a full-stack blog application allowing users to sign up and upload posts. Implemented user authentication using NextAuth and utilized Prisma ORM with PostgreSQL for effective data management. The application is styled with Tailwind CSS and built using Next.js with TypeScript for a modern and maintainable codebase.",
+    image:
+      "https://tse2.mm.bing.net/th?id=OIP.J-6CDaWZL-V8OinqFIZ_LQHaHa&pid=Api&P=0&h=180", // Replace with actual image if available
+    tags: [
+      "Next.js",
+      "Prisma",
+      "PostgreSQL",
+      "Tailwind CSS",
+      "NextAuth",
+    ],
+    category: "full stack",
+    webapp: "https://thinavadhanthi.vercel.app/", // Replace with actual link
+  },
+
+  {
+    id: 4,
+    title: "E-Commerce App",
+    date: "Jun 2024",
+    description:
+      "Built a feature-rich e-commerce application with shopping cart, search functionality, and Stripe payment gateway integration. Implemented user authentication with NextAuth and managed products and orders using Prisma with MongoDB. Designed an admin page with summary and bar graph statistics, and included a user order table for order tracking. Styled the application with Tailwind CSS for a modern look and feel.",
+    image:
+      "https://tse4.mm.bing.net/th?id=OIP.u-1dWGTM7biu51ZxKpOIiAHaFL&pid=Api&P=0&h=180", // Replace with actual image if available
+    tags: [
+      "Next.js",
+      "Prisma",
+      "MongoDB",
+      "Tailwind CSS",
+      "NextAuth",
+      "Stripe",
+    ],
+    category: "full stack",
+    webapp: "https://e-shop-ivory-ten.vercel.app/", // Replace with actual link
+  },
+];
